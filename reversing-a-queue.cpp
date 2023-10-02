caedc17fdc387cc5f9bfd4101a63ea3983229a9b
rizwanhossainrahat <rizwanhossainrahat400@gmail.com>
2023-10-02 14:21:32 +0600

    stack queue problem solve

---
+++ reversing-a-queue.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#include <bits/stdc++.h> 
+queue<int> reverseQueue(queue<int> q)
+{
+    // Write your code here.
+    stack<int>s;
+    while(!q.empty())
+    {
+        s.push(q.front());
+        q.pop();
+    }
+    while(!s.empty())
+    {
+        q.push(s.top());
+        s.pop();
+    }
+    return q;
+}
+
+int main()
+{
+    
+    return 0;
+}
